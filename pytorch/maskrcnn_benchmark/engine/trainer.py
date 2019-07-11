e0fbce41e414043f11c395f032e0b6801fc8a81b
puririshi98 <puririshi98@berkeley.edu>
2019-07-11 12:04:14 -0700

    Update trainer.py

--- pytorch/maskrcnn_benchmark/engine/trainer.py
+++ pytorch/maskrcnn_benchmark/engine/trainer.py
@@ -67,7 +67,7 @@ def do_train(
         images = images.to(device)
         targets = [target.to(device) for target in targets]
 
-        loss_dict = model(images, targets)
+        loss_dict = model.float()(images, targets)
 
         losses = sum(loss for loss in loss_dict.values())
 
